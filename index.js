2e54508efcb923cdb3402e26bfa1c6e54bca62e0
rmaciaszczyk <rmaciaszczyk@zut.edu.pl>
2024-10-21 14:50:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3030;
 
 app.get('/',(req,res) => {
     res.status(200);
-    res.send("Hello World!!");
+    res.send("Hello World!!!");
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT} `));
